5c6fc083b519e37e0ec2a133fc461730fcc64566
JRoffel <jasonroffel@hotmail.nl>
2015-03-06 14:10:15 +0100

    Version 0.5.4
    Commited change to query, caused bug which can be fixed by splitting
    query in 2 parts. Upload of fix will be soon

--- game.php
+++ game.php
@@ -10,7 +10,7 @@
 
 	<body>
 		<?php
-			$query = "select game.name as game_name, platform, developer.name as developer_name, publisher.name as publisher_name from game left join publisher on game.publisher_id = publisher.id left join developer on game.developer_id = developer.id where game.id =".$_GET['id'].";";
+			$query = "select game.name as game_name, platform, developer.name as developer_name, publisher.name as publisher_name, grade, description, website.url as website_url, website.name as website_name from game left join publisher on game.publisher_id = publisher.id left join developer on game.developer_id = developer.id left join review on review.game_id = game.id left join website on review.website_id = website.id where game.id =".$_GET['id'].";";
 
 			$result = $connection->query($query);
 			$rows = $result->fetch_all(MYSQLI_ASSOC);
@@ -22,6 +22,15 @@
 					<li>Ontwikkelaar: ".$row['developer_name']."</li><br>
 					<li>Uitgever: ".$row['publisher_name']."</li><br>
 				<ul>";
+
+				echo "<p class = 'review'><br>
+						<ul>
+							<li>".$row['website_name']."</li><br>
+							<li>".$row['grade']."/100</li><br>
+							<li>".$row['description']."</li><br>
+							<li><a href='".$row['website_url']."'>Lees meer</a></li><br>
+						</ul>
+					<p>";
 			}
 		?>
 	</body>
