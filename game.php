d116fb50b174e4d3db060f85368306efdf3776e3
JRoffel <jasonroffel@hotmail.nl>
2015-03-06 14:24:38 +0100

    Version 0.5.6
    Fixed review problem by splitting up the query in 2 parts, found bugs in
    the CSS file for the H1 elements in Index.php and Game.php, requesting
    fix

--- game.php
+++ game.php
@@ -10,7 +10,7 @@
 
 	<body>
 		<?php
-			$query = "select game.name as game_name, platform, developer.name as developer_name, publisher.name as publisher_name, grade, description, website.url as website_url, website.name as website_name from game left join publisher on game.publisher_id = publisher.id left join developer on game.developer_id = developer.id left join review on review.game_id = game.id left join website on review.website_id = website.id where game.id =".$_GET['id'].";";
+			$query = "select game.name as game_name, platform, developer.name as developer_name, publisher.name as publisher_name from game left join publisher on game.publisher_id = publisher.id left join developer on game.developer_id = developer.id where game.id =".$_GET['id'].";";
 
 			$result = $connection->query($query);
 			$rows = $result->fetch_all(MYSQLI_ASSOC);
@@ -22,13 +22,19 @@
 					<li>Ontwikkelaar: ".$row['developer_name']."</li><br>
 					<li>Uitgever: ".$row['publisher_name']."</li><br>
 				<ul>";
+			}
 
+			$review_query = "select grade, description, review.url as review_url, website.name as website_name from review left join game on review.game_id = game.id left join website on review.website_id = website.id where game.id =".$_GET['id'].";";
+
+			$review_result = $connection->query($review_query);
+			$reviews = $review_result->fetch_all(MYSQLI_ASSOC);
+			foreach($reviews as $review){
 				echo "<p class = 'review'><br>
 						<ul>
-							<li>".$row['website_name']."</li><br>
-							<li>".$row['grade']."/100</li><br>
-							<li>".$row['description']."</li><br>
-							<li><a href='".$row['website_url']."'>Lees meer</a></li><br>
+							<li>".$review['website_name']."</li><br>
+							<li>".$review['grade']."/100</li><br>
+							<li>".$review['description']."</li><br>
+							<li><a href='".$review['review_url']."'>Lees meer</a></li><br>
 						</ul>
 					<p>";
 			}
